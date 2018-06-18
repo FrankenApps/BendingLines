d8cc810e5f988ab99821f62ad8f87495eab05f8d
FrankenApps <de.frankenapps@gmail.com>
2018-06-18 10:32:29 +0200

    Second example almost ready

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
       this.Iy = 1.2;     //MPa
       this.E = 120;         //MPa
       this.l = 400;       //px
-      this.a = 400;      //px
+      this.a = 200;      //px
       this.resolution = '0.1';
       this.colored = false;
       this.Update = function(){ update2(); }
@@ -122,7 +122,7 @@ function update1 (){
   $('#joist1SVG > svg').remove();
   initialize1();
   d3.select('#placeholder1').remove();
-  d3.selectAll('circle').remove();
+  d3.select('#svg1').selectAll('circle').remove();
   var yMax = 0, yF = 0;
   if((joist1.l)<joist1.a){
     yMax = joist1.F/(6*joist1.E*joist1.Iy*10e5)*(3*joist1.a*Math.pow((joist1.l),2)-Math.pow((joist1.l),3));
@@ -277,7 +277,7 @@ function initialize2 (){
       svg2.attr("transform", d3.event.transform);
     }))
     .append("g")
-    .attr("id", "svg1");
+    .attr("id", "svg2");
   } else{
     svg2 = d3.select("#joist2SVG")
     .append("svg")
@@ -359,10 +359,10 @@ function update2(){
   $('#joist2SVG > svg').remove();
   initialize2();
   d3.select('.placeholder2').remove();
-  d3.selectAll('circle').remove();
-  var yMax = joist2.F*(joist2.l-joist2.a)/(9*Math.sqrt(3)*joist2.E*joist2.Iy*joist2.l)*Math.pow(Math.pow(joist2.l,2)-Math.pow(joist2.l-joist2.a,2),3/2);
-  console.log(yMax);
+  d3.select('#svg2').selectAll('circle').remove();
   
+  var m1 = (parseFloat(document.getElementById('joist2SVG').clientWidth)-joist2.l)/2; //margin left/right
+
   var yF = 0;
   if((joist2.l/2)<joist2.a){
     yF = joist2.F*(joist2.l-joist2.a)/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(Math.pow(joist2.l,2)*joist2.a-Math.pow(joist2.l-joist2.a,2)*joist2.a-Math.pow(joist2.a,3));
@@ -370,19 +370,140 @@ function update2(){
     yF=joist2.F*(joist2.l-joist2.a)/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(joist2.l/(joist2.l-joist2.a)*Math.pow(joist2.a-joist2.a,3)+(Math.pow(joist2.l,2)-Math.pow(joist2.l-joist2.a,2))*joist2.a-Math.pow(joist2.a,3));
   }
 
+  //show additional stuff
+  //Dimension: length
+  var yL, yA, marginSign;
+  if(joist2.F < 0){
+    yL = 190;
+    yA = 150;
+    marginSign = -1;
+  } else{
+    yL = 30;
+    yA = 70;
+    marginSign = 1;
+  }
+  svg2.append("line")
+      .attr("x1", m1+10)
+      .attr("y1", yL)
+      .attr("x2", m1-10+joist2.l)
+      .attr("y2", yL)
+      .attr("stroke-width", 1)
+      .attr("stroke", "grey");
 
-  var m1 = (parseFloat(document.getElementById('joist2SVG').clientWidth)-joist2.l)/2; //margin left/right
+  svg2.append('path')
+      .attr('d', function(d) { 
+        var x = m1+joist2.l-15, y = yL;
+        return 'M ' + x +' '+ y + ' l 0 5 l 15 -5 l -15 -5 z';
+      })
+      .style('fill', 'grey');
+
+  svg2.append('path')
+      .attr('d', function(d) { 
+        var x = m1+15, y = yL;
+        return 'M ' + x +' '+ y + ' l 0 5 l -15 -5 l 15 -5 z';
+      })
+      .style('fill', 'grey');
+
+  svg2.append('text')
+      .attr("x", m1+joist1.l/2)
+      .attr("y", yL-10)
+      .text('l')
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "20px")
+      .attr("fill", "grey");
+
+  svg2.append("line")
+      .attr("x1", m1+joist2.l)
+      .attr("y1", yL-20*marginSign)
+      .attr("x2", m1+joist2.l)
+      .attr("y2", 110)
+      .attr("stroke-width", 0.25)
+      .attr("stroke", "grey");
+
+  svg2.append("line")
+      .attr("x1", m1)
+      .attr("y1", yL-20*marginSign)
+      .attr("x2", m1)
+      .attr("y2", 110)
+      .attr("stroke-width", 0.25)
+      .attr("stroke", "grey");
+
+  //force away from origin (a)
+  svg2.append("line")
+      .attr("x1", m1+10)
+      .attr("y1", yA)
+      .attr("x2", m1-10+joist2.a)
+      .attr("y2", yA)
+      .attr("stroke-width", 1)
+      .attr("stroke", "grey");
+
+      svg2.append('path')
+      .attr('d', function(d) { 
+        var x = m1+joist2.a-15, y = yA;
+        return 'M ' + x +' '+ y + ' l 0 5 l 15 -5 l -15 -5 z';
+      })
+      .style('fill', 'grey');
+
+  svg2.append('path')
+      .attr('d', function(d) { 
+        var x = m1+15, y = yA;
+        return 'M ' + x +' '+ y + ' l 0 5 l -15 -5 l 15 -5 z';
+      })
+      .style('fill', 'grey');
+
+  svg2.append('text')
+      .attr("x", m1+joist2.a/2)
+      .attr("y", yA-10)
+      .text('a')
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "20px")
+      .attr("fill", "grey");
+
+  svg2.append("line")
+      .attr("x1", m1+joist2.a)
+      .attr("y1", yA-20*marginSign)
+      .attr("x2", m1+joist2.a)
+      .attr("y2", yF+110)
+      .attr("stroke-width", 0.25)
+      .attr("stroke", "grey");
+
+    //Force
+    svg2.append("line")
+    .attr("x1", m1+joist2.a)
+    .attr("y1", yF-40+110)
+    .attr("x2", m1+joist2.a)
+    .attr("y2", yF+110-7) // instead of -7.5 to remove white line
+    .attr("stroke-width", 4)
+    .attr("stroke", "blue");
+
+svg2.append('path')
+    .attr('d', function(d) { 
+      var x = m1+joist2.a, y = yF+110;
+      return 'M ' + x +' '+ y + ' l -7.5 -7.5 l 15 0 z';
+    })
+    .style('fill', 'blue');
+
+svg2.append('text')
+    .attr("x", m1+10+joist2.a)
+    .attr("y", (yF+110-7-(yF-40+110))/2+(yF-40+110)+7.5)
+    .text('F')
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "20px")
+    .attr("fill", "blue");
+
+  var constant = joist2.F/(6*joist2.E*joist2.Iy*10e5*joist2.l);
+  var a = 0, b = 0;
+  a = joist2.a;
+  b = joist2.l-a;
+  var yMax = joist2.F*b/(9*Math.sqrt(3)*joist2.E*joist2.Iy*joist2.l*10e5)*Math.pow(Math.pow(joist2.l,2)-Math.pow(b,2),1.5);
+  console.log('Durchbiegung ' + yMax + ' a = ' + a + ' b = ' +b);
   for(var x=m1; x<parseFloat(joist2.l)+m1; x+=parseFloat(joist2.resolution)){
     var y = 0;
-    if((x-m1)<joist2.a && joist2.a < joist2.l/2){
-      y=joist2.F*(joist2.l-joist2.a)/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(Math.pow(joist2.l,2)*(x-m1)-Math.pow(joist2.l-joist2.a,2)*(x-m1)-Math.pow((x-m1),3));
-    } else if ((x-m1)>joist2.a && joist2.a < joist2.l/2){
-      y=joist2.F*(joist2.l-joist2.a)/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(joist2.l/(joist2.l-joist2.a)*Math.pow((x-m1)-joist2.a,3)+(Math.pow(joist2.l,2)-Math.pow(joist2.l-joist2.a,2))*(x-m1)-Math.pow((x-m1),3));
-    } /*else if ((x-m1)<(joist2.l-joist2.a) && (joist2.l-joist2.a) < joist2.l/2){
-      y=joist2.F*(joist2.l-(joist2.l-joist2.a))/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(Math.pow(joist2.l,2)*(x-m1)-Math.pow(joist2.l-(joist2.l-joist2.a),2)*(x-m1)-Math.pow((x-m1),3));
-    } else if((x-m1)>(joist2.l-joist2.a) && (joist2.l-joist2.a) < joist2.l/2){
-      y=joist2.F*(joist2.l-(joist2.l-joist2.a))/(6*joist2.E*joist2.Iy*10e5*joist2.l)*(joist2.l/(joist2.l-(joist2.l-joist2.a))*Math.pow((x-m1)-(joist2.l-joist2.a),3)+(Math.pow(joist2.l,2)-Math.pow(joist2.l-(joist2.l-joist2.a),2))*(x-m1)-Math.pow((x-m1),3));
-    } */else{
+    if((x-m1)<a){
+      y=constant*b*(Math.pow(joist2.l,2)*(x-m1)-Math.pow(b,2)*(x-m1)-Math.pow((x-m1),3));
+    } else if ((x-m1)>a){
+      y=constant*b*(joist2.l/b*Math.pow((x-m1)-a,3)+(Math.pow(joist2.l,2)-Math.pow(b,2))*(x-m1)-Math.pow((x-m1),3));
+    } else{
       y = 0;
     }
     var color = '#000000';
